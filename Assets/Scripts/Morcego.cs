acaa6535c144b54212b377d9a06ec66d54b9748f
Giovanna Chiba <gigichiba@gmail.com>
2017-03-07 11:57:21 -0300

    Incluindo o inimigo Morcego

--- Assets/Scripts/Morcego.cs
+++ Assets/Scripts/Morcego.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Morcego : MonoBehaviour {
+	
+	public float velocidade;
+	public float min;
+    public float max;
+    public float espera;
+	
+	
+	public float velocidade2 = 1f;
+	 
 
-	// Use this for initialization
 	void Start () {
 		
+	StartCoroutine(Move(max));
+    }
+
+    IEnumerator Move(float destino)
+    {
+        while (Mathf.Abs(destino - transform.localPosition.y) > 0.2f)
+        {
+            Vector3 direcao = (destino == max) ? Vector3.up : Vector3.down;
+            Vector3 velocidadeVetorial = direcao * velocidade;
+            transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		float x = Time.deltaTime;
+		
+		void Update () {
+        Vector3 velocidadeVectorial1 = Vector3.left * velocidade2;
+
+        transform.localPosition = transform.localPosition + velocidadeVectorial1 * Time.deltaTime;
+
+         
 	}
-}
+		}
